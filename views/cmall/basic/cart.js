81eb1ac3343061812884e5c613cb7bac0fce5ab6
ZINV <zinv@coinmeca.net>
2020-05-20 16:37:55 +0900

    200520 - Update cart

---
+++ views/cmall/basic/cart.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+
+import app from './cart.vue';
+// i18n
+import i18n from '*/src/i18n/i18n'
+
+Vue.config.productionTip = false;
+Vue.prototype.$log = console.log.bind(console);
+
+
+window.vm = new Vue({
+  i18n,
+  render: h => h(app),
+}).$mount('#app')
