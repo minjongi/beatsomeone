60b25e3374bdf43a01d3f32858a906a9b2a9bb55
ZINV <zinv@email.com>
2020-05-21 23:37:52 +0900

    2000521 - Update mypage

--- views/cmall/basic/cart.js
+++ views/cmall/basic/cart.js
@@ -1,14 +1,31 @@
 import Vue from 'vue'
-
+import VueRouter  from 'vue-router';
 import app from './cart.vue';
 // i18n
 import i18n from '*/src/i18n/i18n'
 
+import cart_1 from "./cart";
+import cart_2 from "./cart2";
+import cart_3 from "./cart3";
+Vue.use(VueRouter);
+
 Vue.config.productionTip = false;
 Vue.prototype.$log = console.log.bind(console);
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: cart},
+    { path: '/2', component: cart2},
+    { path: '/3', component: cart3},
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
+});
 
 
 window.vm = new Vue({
   i18n,
+  router,
   render: h => h(app),
 }).$mount('#app')
--- views/cmall/basic/cart.php
+++ views/cmall/basic/cart.php
@@ -1,5 +1,5 @@
-<?php $this->managelayout->add_css(element('view_skin_url', $layout) . '/css/style.css'); ?>
-<?php $this->managelayout->add_js('/dist/js/cart.js'); ?>
+<?php $this->managelayout->add_css('/dist/cart.css'); ?>
+<?php $this->managelayout->add_js('/dist/cart.js'); ?>
 
 
 <?php $this->managelayout->add_script('window.vm.$children[0].$data.isLogin = '.($this->member->is_member() ? 'true' : 'false')); ?>
