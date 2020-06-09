64fe41a1acac0bc366208da70ba9f3a3e4903254
daesony <gonggongplace@gmail.com>
2020-06-09 13:08:09 +0900

    create mobile listitem page

--- views/beatsomeone/mobile/mypage/list_item.php
+++ views/beatsomeone/mobile/mypage/list_item.php
@@ -1,5 +1,5 @@
-<?php $this->managelayout->add_css('/dist/list_item.css'); ?>
-<?php $this->managelayout->add_js('/dist/list_item.js'); ?>
+<?php $this->managelayout->add_css('/dist/m_list_item.css'); ?>
+<?php $this->managelayout->add_js('/dist/m_list_item.js'); ?>
 
 <?php
 $this->managelayout->add_script('window.vm.$children[0].$data.isLogin = ("' . $this->member->is_member() . '" != "");');
