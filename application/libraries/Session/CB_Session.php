498ea40b0075a6586d786228c5032329337d3dd5
minjongi <minjongi@nate.com>
2020-07-08 15:48:17 +0900

    무한로딩 오류 수정

---
+++ application/libraries/Session/CB_Session.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CB_Session extends CI_Session
+{
+    public function set_flashdata($data, $value = NULL)
+    {
+        //
+    }
+}
