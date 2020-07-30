121e942f6f0674f07e701a979f7ea1d27d53229f
eonsang <2eonhworking@gmail.com>
2020-07-30 23:37:12 +0900

    mobile

--- views/beatsomeone/mobile/mypage/component/Parchase.vue
+++ views/beatsomeone/mobile/mypage/component/Parchase.vue
@@ -1,44 +1,46 @@
 <template>
-  <div class="parchase-description">
-    <p>
-      <i>
-        <img src="/assets/images/icon/parchase-info6.png" alt />
-      </i>
-      <!-- {{$t('available60Days')}} -->
-      Profits from performances and can be used in broadcasting
-    </p>
-    <p></p>
-    <p>
-      <i>
-        <img src="/assets/images/icon/parchase-info1.png" alt />
-      </i>
-      <!-- {{$t('available60Days')}} -->
-      Available for 60 days
-    </p>
-    <p>
-      <i>
-        <img src="/assets/images/icon/parchase-info3.png" alt />
-      </i>
-      <!-- {{$t('rentedMembersCannotBeRerentedToOthers')}} -->
-      Unable to register commercial music copyrights
-    </p>
-    <p>
-      <i>
-        <img src="/assets/images/icon/parchase-info2.png" alt />
-      </i>
-      <!-- {{$t('unableToEditArbitrarily')}} -->
-      Only simple cutting editing is possible
-    </p>
-    <p>
-      <i>
-        <img src="/assets/images/icon/parchase-info7.png" alt />
-      </i>
-      <!-- {{$t('noOtherActivitiesNotAuthorizedByThePlatform')}} -->
-      Will continue to be sold to the majority other than this buyer
-    </p>
-    <div class="copybox">
-      <span>Seller's copyright must also be partially recognized when registering music copyrights.</span>
-      <span>If you wish to transfer copyrights, you need to contact the customer center.</span>
+  <div class="option_item basic">
+    <div class="parchase-description">
+      <p>
+        <i>
+          <img src="/assets/images/icon/parchase-info6.png" alt />
+        </i>
+        <!-- {{$t('available60Days')}} -->
+        Profits from performances and can be used in broadcasting
+      </p>
+      <p></p>
+      <p>
+        <i>
+          <img src="/assets/images/icon/parchase-info1.png" alt />
+        </i>
+        <!-- {{$t('available60Days')}} -->
+        Available for 60 days
+      </p>
+      <p>
+        <i>
+          <img src="/assets/images/icon/parchase-info3.png" alt />
+        </i>
+        <!-- {{$t('rentedMembersCannotBeRerentedToOthers')}} -->
+        Unable to register commercial music copyrights
+      </p>
+      <p>
+        <i>
+          <img src="/assets/images/icon/parchase-info2.png" alt />
+        </i>
+        <!-- {{$t('unableToEditArbitrarily')}} -->
+        Only simple cutting editing is possible
+      </p>
+      <p>
+        <i>
+          <img src="/assets/images/icon/parchase-info7.png" alt />
+        </i>
+        <!-- {{$t('noOtherActivitiesNotAuthorizedByThePlatform')}} -->
+        Will continue to be sold to the majority other than this buyer
+      </p>
+      <div class="copybox">
+        <span>Seller's copyright must also be partially recognized when registering music copyrights.</span>
+        <span>If you wish to transfer copyrights, you need to contact the customer center.</span>
+      </div>
     </div>
   </div>
 </template>
