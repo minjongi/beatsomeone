156942e6140ce5ac75cd037e65e3229ed52b90c0
fullstack0220 <fullstack0220@outlook.com>
2020-09-21 14:22:45 +0800

    billing

--- views/beatsomeone/mobile/cart/BillingNew.vue
+++ views/beatsomeone/mobile/cart/BillingNew.vue
@@ -550,6 +550,9 @@
             this.$set(this.allatForm, 'buyer_nm', mem_name);
             this.$set(this.allatForm, 'pmember_id', this.member.mem_userid);
             let address = this.member.mem_address1 + ' ' + this.member.mem_address2 + ' ' + this.member.mem_address3 + ' ' + this.member.mem_address4;
+            if (!address) {
+                address = this.member.mem_email;
+            }
             this.$set(this.allatForm, 'recp_addr', address);
             this.$set(this.allatForm, 'recp_nm', mem_name);
         },
