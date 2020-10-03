16d630ce429b296774d639ca4216b9ae3e05117f
fullstack0220 <fullstack0220@outlook.com>
2020-10-03 20:50:00 +0800

    정산내역 관리자페지

--- application/controllers/admin/cmall/Settlement.php
+++ application/controllers/admin/cmall/Settlement.php
@@ -1,12 +1,13 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Settlement extends CB_Controller {
+class Settlement extends CB_Controller
+{
     /**
      * 관리자 페이지 상의 현재 디렉토리입니다
      * 페이지 이동시 필요한 정보입니다
      */
-    public $pagedir = 'cmall/cmallsettlement';
+    public $pagedir = 'cmall/settlement';
 
     /**
      * 모델을 로딩합니다
@@ -42,7 +43,7 @@ class Settlement extends CB_Controller {
          * 페이지에 숫자가 아닌 문자가 입력되거나 1보다 작은 숫자가 입력되면 에러 페이지를 보여줍니다.
          */
         $param =& $this->querystring;
-        $page = (((int) $this->input->get('page')) > 0) ? ((int) $this->input->get('page')) : 1;
+        $page = (((int)$this->input->get('page')) > 0) ? ((int)$this->input->get('page')) : 1;
         $findex = $this->input->get('findex') ? $this->input->get('findex') : $this->{$this->modelname}->primary_key;
         $forder = $this->input->get('forder', null, 'desc');
         $sfield = $this->input->get('sfield', null, '');
@@ -57,23 +58,50 @@ class Settlement extends CB_Controller {
         $this->{$this->modelname}->allow_search_field = array('mll_id', 'mll_userid', 'mll_success', 'member_login_log.mem_id', 'mll_datetime', 'mll_ip', 'mll_reason', 'mll_log_agent', 'mll_url', 'mll_referer'); // 검색이 가능한 필드
         $this->{$this->modelname}->search_field_equal = array('mll_id', 'member_login_log.mem_id'); // 검색중 like 가 아닌 = 검색을 하는 필드
         $this->{$this->modelname}->allow_order_field = array('mll_id'); // 정렬이 가능한 필드
-        $result = $this->{$this->modelname}
-            ->get_admin_list($per_page, $offset, '', '', $findex, $forder, $sfield, $skeyword);
+        $result = array();
+        $sql = "SELECT COUNT(*) as total_rows FROM cb_cmall_order_detail AS cod
+         LEFT JOIN cb_cmall_settlement_history AS csh ON csh.cod_id = cod.cod_id
+         LEFT JOIN cb_cmall_order as co ON co.cor_id = cod.cor_id
+         LEFT JOIN cb_cmall_item as ci ON cod.cit_id = ci.cit_id
+         LEFT JOIN cb_cmall_item_detail as cid ON cid.cde_id = cod.cde_id
+         LEFT JOIN cb_member as m ON m.mem_id = ci.mem_id
+         LEFT JOIN cb_member_group_member as mgm ON m.mem_id = mgm.mem_id
+         LEFT JOIN cb_member_group as mg ON mg.mgr_id = mgm.mgr_id";
+        $total_rows = ($this->db->query($sql)->row_array())['total_rows'];
+        $result['total_rows'] = $total_rows;
+//        $result = $this->{$this->modelname}
+//            ->get_admin_list($per_page, $offset, '', '', $findex, $forder, $sfield, $skeyword);
+        $sql = "SELECT cod.cod_id, co.cor_datetime,
+       co.mem_nickname                                          as buyer_nickname,
+       m.mem_nickname                                           as seller_nickname,
+       ci.cit_file_1,
+       CONCAT(ci.cit_name, '(', cid.cde_title, ')')             as item_name,
+       co.cor_pg,
+       CASE
+           WHEN co.cor_pg = 'allat' THEN cid.cde_price
+           WHEN co.cor_pg = 'paypal' THEN cid.cde_price_d
+           ELSE cid.cde_price END                               as total_money,
+       cod.cod_status,
+       csh.csh_datetime,
+       IFNULL(csh.csh_settle_money, CASE
+                                        WHEN co.cor_pg = 'allat' THEN cid.cde_price
+                                        WHEN co.cor_pg = 'paypal' THEN cid.cde_price_d
+                                        ELSE cid.cde_price END) * (100 - mg.mgr_commission) / 100.0 as csh_settle_money,
+       csh.csh_status,
+       mg.mgr_description,
+       mg.mgr_commission
+FROM cb_cmall_order_detail AS cod
+         LEFT JOIN cb_cmall_settlement_history AS csh ON csh.cod_id = cod.cod_id
+         LEFT JOIN cb_cmall_order as co ON co.cor_id = cod.cor_id
+         LEFT JOIN cb_cmall_item as ci ON cod.cit_id = ci.cit_id
+         LEFT JOIN cb_cmall_item_detail as cid ON cid.cde_id = cod.cde_id
+         LEFT JOIN cb_member as m ON m.mem_id = ci.mem_id
+         LEFT JOIN cb_member_group_member as mgm ON m.mem_id = mgm.mem_id
+         LEFT JOIN cb_member_group as mg ON mg.mgr_id = mgm.mgr_id";
+        $result['list'] = $this->db->query($sql)->result_array();
         $list_num = $result['total_rows'] - ($page - 1) * $per_page;
         if (element('list', $result)) {
             foreach (element('list', $result) as $key => $val) {
-                $result['list'][$key]['display_name'] = display_username(
-                    element('mem_userid', $val),
-                    element('mem_nickname', $val),
-                    element('mem_icon', $val)
-                );
-                if (element('mll_useragent', $val)) {
-                    $userAgent = get_useragent_info(element('mll_useragent', $val));
-                    $result['list'][$key]['browsername'] = $userAgent['browsername'];
-                    $result['list'][$key]['browserversion'] = $userAgent['browserversion'];
-                    $result['list'][$key]['os'] = $userAgent['os'];
-                    $result['list'][$key]['engine'] = $userAgent['engine'];
-                }
                 $result['list'][$key]['num'] = $list_num--;
             }
         }
@@ -111,5 +139,73 @@ class Settlement extends CB_Controller {
         $this->data = $view;
         $this->layout = element('layout_skin_file', element('layout', $view));
         $this->view = element('view_skin_file', element('layout', $view));
+    }
+
+    public function settle()
+    {
+        // 이벤트 라이브러리를 로딩합니다
+        $eventname = 'event_admin_cmall_settlement_settle';
+        $this->load->event($eventname);
+
+        // 이벤트가 존재하면 실행합니다
+        Events::trigger('before', $eventname);
+
+        if ($this->input->post('chk') && is_array($this->input->post('chk'))) {
+            foreach ($this->input->post('chk') as $cod_id) {
+                if ($cod_id) {
+                    $sql = "SELECT * FROM cb_cmall_settlement_history WHERE cod_id = ?";
+                    $row = $this->db->query($sql, [$cod_id])->row_array();
+                    $sql2 = "SELECT cod.cod_id, co.cor_datetime,
+                                   co.mem_nickname                                          as buyer_nickname,
+                                   m.mem_nickname                                           as seller_nickname,
+                                   ci.cit_file_1,
+                                   CONCAT(ci.cit_name, '(', cid.cde_title, ')')             as item_name,
+                                   co.cor_pg,
+                                   CASE
+                                       WHEN co.cor_pg = 'allat' THEN cid.cde_price
+                                       WHEN co.cor_pg = 'paypal' THEN cid.cde_price_d
+                                       ELSE cid.cde_price END                               as total_money,
+                                   cod.cod_status,
+                                   csh.csh_datetime,
+                                   IFNULL(csh.csh_settle_money, CASE
+                                                                    WHEN co.cor_pg = 'allat' THEN cid.cde_price
+                                                                    WHEN co.cor_pg = 'paypal' THEN cid.cde_price_d
+                                                                    ELSE cid.cde_price END) * (100 - mg.mgr_commission) / 100.0 as csh_settle_money,
+                                   csh.csh_status,
+                                   mg.mgr_description,
+                                   mg.mgr_commission
+                            FROM cb_cmall_order_detail AS cod
+                                     LEFT JOIN cb_cmall_settlement_history AS csh ON csh.cod_id = cod.cod_id
+                                     LEFT JOIN cb_cmall_order as co ON co.cor_id = cod.cor_id
+                                     LEFT JOIN cb_cmall_item as ci ON cod.cit_id = ci.cit_id
+                                     LEFT JOIN cb_cmall_item_detail as cid ON cid.cde_id = cod.cde_id
+                                     LEFT JOIN cb_member as m ON m.mem_id = ci.mem_id
+                                     LEFT JOIN cb_member_group_member as mgm ON m.mem_id = mgm.mem_id
+                                     LEFT JOIN cb_member_group as mg ON mg.mgr_id = mgm.mgr_id WHERE cod.cod_id = ?";
+                    $row2 = $this->db->query($sql2, [$cod_id])->row_array();
+                    $total_money = $row2['total_money'];
+                    $pg = $row2['cor_pg'];
+                    if ($row) {
+                        $sql3 = "UPDATE cb_cmall_settlement_history SET pg=?, csh_datetime=NOW(), csh_settle_money=?, csh_status=1 WHERE cod_id=?";
+                        $this->db->query($sql3, [$pg, $total_money, $cod_id]);
+                    } else {
+                        $sql3 = "INSERT INTO cb_cmall_settlement_history (cod_id, pg, csh_datetime, csh_settle_money, csh_status) VALUES (?, ?, NOW(), ?, 1)";
+                        $this->db->query($sql3, [$cod_id, $pg, $total_money]);
+                    }
+                }
+            }
+        }
+
+        // 이벤트가 존재하면 실행합니다
+        Events::trigger('after', $eventname);
+
+        /**
+         * 삭제가 끝난 후 목록페이지로 이동합니다
+         */
+        $this->session->set_flashdata(
+            'message',
+            '정상적으로 삭제되었습니다'
+        );
+        redirect(admin_url($this->pagedir));
     }
 }
--- views/admin/basic/cmall/settlement/index.php
+++ views/admin/basic/cmall/settlement/index.php
@@ -10,9 +10,7 @@
             ob_start();
             ?>
             <div class="btn-group pull-right" role="group" aria-label="...">
-                <button type="button" class="btn btn-outline btn-primary btn-sm" onclick="doSettlement()">정산하기</button>
-                <a href="<?php echo element('listall_url', $view); ?>" class="btn btn-outline btn-default btn-sm">전체목록</a>
-                <button type="button" class="btn btn-outline btn-default btn-sm btn-list-delete btn-list-selected disabled" data-list-delete-url = "<?php echo element('list_delete_url', $view); ?>" >선택삭제</button>
+                <button type="button" class="btn btn-outline btn-primary btn-sm" data-url="/admin/cmall/settlement/settle" onclick="doSettlement(event)">정산하기</button>
             </div>
             <?php
             $buttons = ob_get_contents();
@@ -25,13 +23,20 @@
                 <thead>
                 <tr>
                     <th>번호</th>
-                    <th>회원아이디</th>
-                    <th>트랙명</th>
-                    <th>날짜</th>
-                    <th>총액(원)</th>
-                    <th>총액($)</th>
-                    <th>상태</th>
-                    <th><input type="checkbox" name="chkall" id="chkall" /></th>
+                    <th>구매일시</th>
+                    <th>판매자 닉네임</th>
+                    <th>판매자 그룹</th>
+                    <th>이미지</th>
+                    <th>주문 상품 정보</th>
+                    <th>구매자 닉네임</th>
+                    <th>PG</th>
+                    <th>총 구매 금액</th>
+                    <th>주문 상태</th>
+                    <th>수수료</th>
+                    <th>정산 금액</th>
+                    <th>정산 상태</th>
+                    <th>정산 일시</th>
+                    <th><input type="checkbox" name="chkall" id="chkall"/></th>
                 </tr>
                 </thead>
                 <tbody>
@@ -41,18 +46,73 @@
                         ?>
                         <tr>
                             <td><?php echo number_format(element('num', $result)); ?></td>
-                            <td><a href="?sfield=member_login_log.mem_id&amp;skeyword=<?php echo element('mem_id', $result); ?>"><?php echo html_escape(element('mem_userid', $result)); ?></a></td>
-                            <td><?php echo display_datetime(element('csh_status', $result), 'full'); ?></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td><input type="checkbox" name="chk[]" class="list-chkbox" value="<?php echo element(element('primary_key', $view), $result); ?>" /></td>
+                            <td><?php echo element('cor_datetime', $result); ?></td>
+                            <td><?php echo element('seller_nickname', $result); ?></td>
+                            <td><?php echo element('mgr_description', $result); ?></td>
+                            <td>
+                                <?php if (element('cit_file_1', $result)) { ?>
+                                    <img src="<?php echo thumb_url('cmallitem', element('cit_file_1', $result), 80); ?>"
+                                         alt="" class="thumbnail mg0" style="width:80px;"/>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php echo element('item_name', $result); ?>
+                            </td>
+                            <td><?php echo element('buyer_nickname', $result); ?></td>
+                            <td>
+                                <?php echo element('cor_pg', $result); ?>
+                            </td>
+                            <td>
+                                <?php
+                                if (strcasecmp(element('cor_pg', $result), 'allat') == 0) {
+                                    echo '₩ ' . number_format(element('total_money', $result));
+                                } else {
+                                    echo '$ ' . number_format(element('total_money', $result), 2);
+                                }
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                if (strcasecmp(element('cod_status', $result), 'order') == 0) {
+                                    echo '결제완료';
+                                } elseif (strcasecmp(element('cod_status', $result), 'deposit') == 0) {
+                                    echo '입금대기';
+                                } elseif (strcasecmp(element('cod_status', $result), 'cancel') == 0) {
+                                    echo '결제취소';
+                                }
+                                ?>
+                            </td>
+                            <td>
+                                <?php echo element('mgr_commission', $result); ?> %
+                            </td>
+                            <td>
+                                <?php
+                                if (strcasecmp(element('cor_pg', $result), 'allat') == 0) {
+                                    echo '₩ ' . number_format(element('csh_settle_money', $result));
+                                } else {
+                                    echo '$ ' . number_format(element('csh_settle_money', $result), 2);
+                                }
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                if (element('csh_status', $result) == null) {
+                                    echo '정산대기';
+                                } else {
+                                    echo '정산완료';
+                                }
+                                ?>
+                            </td>
+                            <td>
+                                <?php echo element('csh_datetime', $result); ?>
+                            </td>
+                            <td><input type="checkbox" name="chk[]" class="list-chkbox"
+                                       value="<?php echo element('cod_id', $result); ?>"/></td>
                         </tr>
                         <?php
                     }
                 }
-                if ( ! element('list', element('data', $view))) {
+                if (!element('list', element('data', $view))) {
                     ?>
                     <tr>
                         <td colspan="8" class="nopost">자료가 없습니다</td>
@@ -65,7 +125,7 @@
         </div>
         <div class="box-info">
             <?php echo element('paging', $view); ?>
-            <div class="pull-left ml20"><?php echo admin_listnum_selectbox();?></div>
+            <div class="pull-left ml20"><?php echo admin_listnum_selectbox(); ?></div>
             <?php echo $buttons; ?>
         </div>
         <?php echo form_close(); ?>
@@ -74,11 +134,13 @@
         <div class="box-search">
             <div class="row">
                 <div class="col-md-6 col-md-offset-3">
-                    <select class="form-control" name="sfield" >
+                    <select class="form-control" name="sfield">
                         <?php echo element('search_option', $view); ?>
                     </select>
                     <div class="input-group">
-                        <input type="text" class="form-control" name="skeyword" value="<?php echo html_escape(element('skeyword', $view)); ?>" placeholder="Search for..." />
+                        <input type="text" class="form-control" name="skeyword"
+                               value="<?php echo html_escape(element('skeyword', $view)); ?>"
+                               placeholder="Search for..."/>
                         <span class="input-group-btn">
 							<button class="btn btn-default btn-sm" name="search_submit" type="submit">검색!</button>
 						</span>
@@ -89,7 +151,12 @@
     </form>
 </div>
 <script>
-    function doSettlement() {
-
+    function doSettlement(event) {
+        if ($("input[name='chk[]']:checked", $('#flist')).length < 1) {
+            alert('자료를 하나 이상 선택하세요.');
+            return;
+        }
+        $('#flist').attr('action', event.target.dataset.url);
+        $('#flist').submit();
     }
 </script>
