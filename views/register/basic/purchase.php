4ae9232e7545ac3387f0bba97ed4f31f9e0d6e2e
redsunsetco <redsunsetco@outlook.com>
2020-12-23 20:42:44 +0800

    fix

--- views/register/basic/purchase.php
+++ views/register/basic/purchase.php
@@ -13,6 +13,7 @@ if (intval($c) > 4) {
             if(result_cd != '0000') {
                 alert(result_cd + ' +: ' + result_msg);
             } else {
+                alert(result_cd + ' +: ' + result_msg);
                 fm1.allat_enc_data.value = enc_data;
                 fm1.action = 'https://qa.beatsomeone.com/pg/allat/subscribe';
                 fm1.method = 'post';
@@ -24,12 +25,16 @@ if (intval($c) > 4) {
     ");
 } else {
     $this->managelayout->add_js('https://tx.allatpay.com/common/NonAllatPayREPlus.js');
-    $sc = "window.allat_shop_receive_url = '".site_url('pg/allat/proc')."';window.selectedGroup = ".json_encode(element('selectedGroup', $view)).";</script>";
-    $this->managelayout->add_script($sc);
-    $sc = "function result_submit(result_cd, result_msg, enc_data) {
-                var index = vm."."$"."children[0]".".$"."children.findIndex(x => x."."$"."vnode.tag.includes('PurchaseMembership'));
-                vm."."$"."children"."["."0"."]".".$"."children[index].procCompletePay(result_cd, result_msg, enc_data);
-            }";
-    $this->managelayout->add_script($sc);
+    $this->managelayout->add_script("
+        function result_submit(result_cd, result_msg, enc_data) {
+            var index = vm.$children[0].$children.findIndex(x => x.$vnode.tag.includes('PurchaseMembership'));
+            vm.$children[0].$children[index].procCompletePay(result_cd, result_msg, enc_data);
+        }
+    ");
 }
 ?>
+
+<script>
+    window.allat_shop_receive_url = '<?= site_url('pg/allat/proc') ?>';
+    window.selectedGroup = <?php echo json_encode(element('selectedGroup', $view)); ?>;
+</script>
