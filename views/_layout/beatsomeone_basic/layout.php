5305b4eb066d518e07513b63f80956673f19716a
minjongi <minjongi@nate.com>
2020-12-23 12:15:02 +0900

    수정사항 처리

--- views/_layout/beatsomeone_basic/layout.php
+++ views/_layout/beatsomeone_basic/layout.php
@@ -19,7 +19,13 @@ $this->managelayout->add_script('window.vm.$i18n = "' . element('cit_id', $view)
     <meta property="og:url" content="<?php echo html_escape(element('og_url', $layout)); ?>"/>
     <meta property="og:description" content="<?php echo html_escape(element('og_description', $layout)); ?>"/>
     <meta property="og:title" content="<?php echo html_escape(element('og_title', $layout)); ?>"/>
-
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WJ6TSGL');</script>
+    <!-- End Google Tag Manager -->
     <script type="application/ld+json">
         {
             "@context": "https://schema.org",
--- views/_layout/beatsomeone_mobile/layout.php
+++ views/_layout/beatsomeone_mobile/layout.php
@@ -21,7 +21,13 @@
   <meta property="og:description" content="<?php echo html_escape(element('og_description', $layout)); ?>"/>
   <meta property="og:title" content="<?php echo html_escape(element('og_title', $layout)); ?>"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WJ6TSGL');</script>
+  <!-- End Google Tag Manager -->
   <script type="application/ld+json">
         {
             "@context": "https://schema.org",
