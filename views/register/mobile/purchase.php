b67eb7a65dea7b4edab66c70b4eed374499f0bc2
redsunsetco <redsunsetco@outlook.com>
2020-12-30 23:13:17 +0800

    fix

--- views/register/mobile/purchase.php
+++ views/register/mobile/purchase.php
@@ -5,7 +5,6 @@ $a = $_SERVER['QUERY_STRING'];
 $b = explode('&', $a)[0];
 $c = explode('=', $b)[1];
 if (intval($c) > 4) {
-    $this->managelayout->add_js('https://tx.allatpay.com/common/AllatPayM.js');
     $this->managelayout->add_script("
         function result_submit(result_cd,result_msg,enc_data) {
             Allat_Mobile_Close();
@@ -22,17 +21,15 @@ if (intval($c) > 4) {
     
     ");
 } else {
-    $this->managelayout->add_js('https://tx.allatpay.com/common/AllatPayM.js');
     $this->managelayout->add_script("
         function result_submit(result_cd, result_msg, enc_data) {
-            var index = vm."."$"."children[0]."."$"."children.findIndex(x => x."."$"."vnode.tag.includes('PurchaseMembership'));
-            vm."."$"."children[0]."."$"."children[index].procCompletePay(result_cd, result_msg, enc_data);
+            vm."."$"."children[0].procCompletePay(result_cd, result_msg, enc_data);
         }
     ");
 }
 
 ?>
-
+<script charset="euc-kr" type="text/javascript" src="https://tx.allatpay.com/common/AllatPayM.js"></script>
 <script>
 window.allat_shop_receive_url = '<?= site_url('pg/allat/proc') ?>';
 window.selectedGroup = <?php echo json_encode(element('selectedGroup', $view)); ?>;
