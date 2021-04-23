8cc356ffa096671d554bf54ed9c9fe09d6cda522
minjongi <minjongi@nate.com>
2021-04-23 12:55:39 +0900

    이벤트 페이지 작업

--- views/beatsomeone/mobile/event/event.php
+++ views/beatsomeone/mobile/event/event.php
@@ -1,5 +1,6 @@
 <?php $this->managelayout->add_css('/dist/m_event.css'); ?>
 <?php $this->managelayout->add_js('/dist/m_event.js'); ?>
+<?php $this->managelayout->add_js('https://dnh523js9661q.cloudfront.net/apis/apTracker.v3.js?v=0413'); ?>
 <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
 <script>
     Kakao.init('4436e30ce5d08f34f5dff5f52d441a37')
